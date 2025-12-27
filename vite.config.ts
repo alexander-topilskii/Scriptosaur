c6909d07eb748fc6fb23aa4cd4167a533c2fcbbe
alexander.topilskii <alexander.topilskii@gmail.com>
2025-12-27 10:01:54 +0200

    inita

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are linked correctly on GitHub Pages (subpath)
+  base: '/Scriptosaur/', // Ensures assets are linked correctly on GitHub Pages (subpath)
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
   }
